36038b97c6ca3a541a9fffeb4cb8a60d71784753
BernardoSa01 <bernardo_nf@hotmail.com>
2026-03-31 12:59:48 -0300

    (statement-service) - feat: expose GET endpoints for statement queries

--- services/statement-service/src/main/java/com/example/statement_service/controller/StatementController.java
+++ services/statement-service/src/main/java/com/example/statement_service/controller/StatementController.java
@@ -4,10 +4,10 @@ import com.example.statement_service.StatementService.StatementService;
 import com.example.statement_service.dto.StatementRequestDTO;
 import com.example.statement_service.model.Statement;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/statements")
@@ -25,5 +25,24 @@ public class StatementController {
         Statement saved = service.save(dto);
 
         return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Statement>> getStatementsByAccount(@RequestParam String accountId) {
+
+        List<Statement> statements = service.getStatementsByAccount(accountId);
+
+        return ResponseEntity.ok(statements);
+    }
+
+    @GetMapping("/period")
+    public ResponseEntity<List<Statement>> getStatementsByTimestamp(
+            @RequestParam String accountId,
+            @RequestParam LocalDateTime start,
+            @RequestParam LocalDateTime end)  {
+
+        List<Statement> statements = service.getStatementsByPeriod(accountId, start, end);
+
+        return ResponseEntity.ok(statements);
     }
 }
