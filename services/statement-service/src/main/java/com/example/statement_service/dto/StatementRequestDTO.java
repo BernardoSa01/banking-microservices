4f860c43574bb00f38f3a296181431bab3e99055
BernardoSa01 <bernardo_nf@hotmail.com>
2026-03-30 21:04:50 -0300

    (statement-service) - feat: add statement request DTO

---
+++ services/statement-service/src/main/java/com/example/statement_service/dto/StatementRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.statement_service.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class StatementRequestDTO {
+
+    private String transactionId;
+    private String accountId;
+    private Long amount;
+    private String type;
+    private String status;
+    private LocalDateTime timestamp;
+}
