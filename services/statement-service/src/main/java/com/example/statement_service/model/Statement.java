73a2dcd175bb9654eb39fe57a259b47133735542
BernardoSa01 <bernardo_nf@hotmail.com>
2026-03-30 21:04:17 -0300

    (statement-service) - feat: define statement document model

---
+++ services/statement-service/src/main/java/com/example/statement_service/model/Statement.java
@@ -0,0 +1,29 @@
+package com.example.statement_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "statements")
+public class Statement {
+    @Id
+    private String id;
+
+    private String transactionId;
+    private String accountId;
+    private Long amount;
+    private String type;
+    private String status;
+    private LocalDateTime timestamp;
+
+
+}
