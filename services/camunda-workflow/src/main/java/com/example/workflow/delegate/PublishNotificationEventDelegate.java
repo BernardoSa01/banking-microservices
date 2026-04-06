f1f17e80348ebe87804391d7f91e1ee31914355c
BernardoSa01 <bernardo_nf@hotmail.com>
2026-04-06 08:45:15 -0300

    (camunda-workflow) - feat: publish notification-events to kafka after transaction workflow

--- services/camunda-workflow/src/main/java/com/example/workflow/delegate/PublishNotificationEventDelegate.java
+++ services/camunda-workflow/src/main/java/com/example/workflow/delegate/PublishNotificationEventDelegate.java
@@ -1,21 +1,40 @@
 package com.example.workflow.delegate;
 
+import com.example.workflow.kafka.NotificationEvent;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class PublishNotificationEventDelegate implements JavaDelegate {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public PublishNotificationEventDelegate(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @Override
     public void execute(DelegateExecution execution) {
 
         String transactionId = (String) execution.getVariable("transactionId");
         String accountId = (String) execution.getVariable("accountId");
+        Long amount = (Long) execution.getVariable("amount");
+        String type = (String) execution.getVariable("type");
 
-        System.out.println("Publishing notification event for transaction: " + transactionId);
-        System.out.println("AccountId: " + accountId);
+        NotificationEvent event = new NotificationEvent();
+        event.setTransactionId(transactionId);
+        event.setAccountId(accountId);
+        event.setAmount(amount);
+        event.setType(type);
+        event.setTimestamp(LocalDateTime.parse(LocalDateTime.now().toString()));
 
-        // publicar no kafka
+        kafkaTemplate.send("notification-events", event);
+
+
+        System.out.println("Notification event published for account: " + accountId);
     }
 }
