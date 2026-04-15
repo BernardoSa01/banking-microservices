24b0bf08df0ea2195ee5852d6b9c58acaccf02e9
BernardoSa01 <bernardo_nf@hotmail.com>
2026-04-15 12:14:33 -0300

    (notification-service) - refactor: improve kafka event logging with topic and transactionId

--- services/notification-service/src/main/java/com/example/notification_service/kafka/NotificationConsumer.java
+++ services/notification-service/src/main/java/com/example/notification_service/kafka/NotificationConsumer.java
@@ -2,11 +2,15 @@ package com.example.notification_service.kafka;
 
 import com.example.notification_service.model.NotificationEvent;
 import com.example.notification_service.service.NotificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(NotificationConsumer.class);
 
     private final NotificationService notificationService;
 
@@ -17,7 +21,11 @@ public class NotificationConsumer {
     @KafkaListener(topics = "notification-events", groupId = "notification-service")
     public void consume(NotificationEvent event) {
 
-        System.out.println("EVENT RECEIVED FROM KAFKA: " + event.getAccountId());
+        //System.out.println("EVENT RECEIVED FROM KAFKA: " + event.getAccountId());
+
+        log.info("Kafka event consumed | topic = notification-events | transactionId ={} | accountdId ={}",
+                event.getTransactionId(),
+                event.getAccountId());
 
         notificationService.send(event.getAccountId(), event);
     }
